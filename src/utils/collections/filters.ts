ffdc7d4d36c0816620a3a70825b9656560c04334
lazosee <oseelazaro@gmail.com>
2026-01-25 18:09:38 +0300

    Update draft filter for posts

--- src/utils/collections/filters.ts
+++ src/utils/collections/filters.ts
@@ -2,5 +2,5 @@ import type { CollectionEntry } from 'astro:content'
 
 export const postsFilter = {
 	'filter-published-on-production': ({ data }: CollectionEntry<'posts'>) =>
-		import.meta.env.DEV ? true : data.isDraft,
+		import.meta.env.NODE_ENV === 'production' ? data.isDraft : true,
 }
