2af63b27b91d46863695c63113df2d052b16989a
lazosee <oseelazaro@gmail.com>
2026-01-25 18:13:35 +0300

    Update filter

--- src/utils/collections/filters.ts
+++ src/utils/collections/filters.ts
@@ -1,6 +1,9 @@
 import type { CollectionEntry } from 'astro:content'
+import { config } from 'dotenv'
+
+config()
 
 export const postsFilter = {
 	'filter-published-on-production': ({ data }: CollectionEntry<'posts'>) =>
-		import.meta.env.NODE_ENV === 'production' ? data.isDraft : true,
+		(process.env.NODE_ENV || import.meta.env.NODE_ENV) === 'production' ? data.isDraft : true,
 }
